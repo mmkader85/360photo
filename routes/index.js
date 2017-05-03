71ed3ecb96a0b38f984858efbff63b42fde1e5f7
Abdul Kader <abdul@propertyguru.com.sg>
2017-05-03 11:19:45 +0800

    Line, Sphere & Custom position versions

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index',
+router.get(['/', '/v1'], function(req, res, next) {
+  res.render('index-v1',
       {
-          title: '360 Photo in Web VR'
+          title: '360 Photo in Web VR - Line tiles'
       }
   );
+});
+
+router.get('/v2', function(req, res, next) {
+    res.render('index-v2',
+        {
+            title: '360 Photo in Web VR - Sphere tiles'
+        }
+    );
+});
+
+router.get('/v3', function(req, res, next) {
+    res.render('index-v3',
+        {
+            title: '360 Photo in Web VR - Custom tiles'
+        }
+    );
 });
 
 router.get('/experiment', function(req, res, next) {
